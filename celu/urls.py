577402e85985b27374c75dce6be9ca468c9526c4
Pedro <pe.arrate@alumnos.duoc.cl>
2020-11-07 17:25:11 -0300

    Trabaja en Tucelu.html deja carrusel ok

--- celu/urls.py
+++ celu/urls.py
@@ -3,7 +3,8 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
+    
     path('', views.celu_home, name='Celu_Home'),
-    path('', views.celu_list, name='Celu_list')
-  
+    path('Celu_list/', views.celu_list, name='Celu_list')
+ 
 ]
