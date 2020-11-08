6e1f5ae822e0843a1b035d97c2baaf8757af8a9d
ArielMarihuen <Ar.marihuen@alumnos.duoc.cl>
2020-11-08 20:36:18 -0300

    Logica de login

--- celu/urls.py
+++ celu/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.Celu_home, name='Celu_Home'),
     path('Acce_list', views.Acce_list , name = 'Acce_list'),
     path('Celu_list', views.Celu_list , name = 'Celu_list'),
-    path('Login_list2', views.Login_list2, name= 'Login_list2')
-
+    path('Login_list2', views.Login_list2 , name = 'Login_list2'),
+    path('Logout', views.logout, name='logout'),
 ]
