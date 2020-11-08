20f2e3986453a8d666a08a4a54fa258ee680e078
ArielMarihuen <Ar.marihuen@alumnos.duoc.cl>
2020-11-07 23:25:42 -0300

    Clonacion de Master

---
+++ celu/urls.py
@@ -0,0 +1,12 @@
+from django.urls.conf import path
+from django.urls import re_path
+from . import views
+
+urlpatterns = [
+    
+    path('', views.Celu_home, name='Celu_Home'),
+    path('Acce_list', views.Acce_list , name = 'Acce_list'),
+    path('Celu_list', views.Celu_list , name = 'Celu_list'),
+    path('Login_list2', views.Login_list2, name= 'Login_list2')
+
+]
---
+++ celu/views.py
@@ -0,0 +1,18 @@
+from django.http import request
+from django.shortcuts import render
+from .models import Celular
+from django.utils import timezone
+
+# Create your views here.
+
+def Celu_home(request):
+    return render(request, 'celu/Tucelu.html')
+
+def Acce_list(request):
+    return render(request, 'celu/Accesorios.html')
+
+def Celu_list(request):
+    return render(request, 'celu/Celu.html')
+
+def Login_list2(request):
+    return render(request, 'celu/Login.html')
