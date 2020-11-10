6c5f84b208b317e2c22165212b2e85b67e624c28
ArielMarihuen <Ar.marihuen@alumnos.duoc.cl>
2020-11-09 21:23:33 -0300

    actualizando tabla

--- celu/models.py
+++ celu/models.py
@@ -8,11 +8,12 @@ class Celular(models.Model):
     modelo = models.CharField(max_length=200)
     marca = models.CharField(max_length=200)
     sistemaoperativo = models.CharField(max_length=200)
-    color = models.CharField(max_length=200)
+    color = models.CharField(max_length=50)
     descripcion = models.TextField(max_length=200, null=True)
     cantidad = models.IntegerField()
     valorneto = models.IntegerField()
     fecharecepcion = models.DateField(blank=True, null=True)
+    foto = models.ImageField(upload_to='picture/celulares')
 
     def publish(self):
         self.fecharecepcion = timezone.now()
