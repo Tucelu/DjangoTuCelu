4ab8a48d8a78c1c4950fe644209bd2a3927c4e0d
ArielMarihuen <Ar.marihuen@alumnos.duoc.cl>
2020-11-10 16:51:35 -0300

    Añadiendo clase accesorios

--- celu/models.py
+++ celu/models.py
@@ -21,3 +21,22 @@ class Celular(models.Model):
 
     def __str__(self):
         return self.modelo
+
+
+class Accesorios(models.Model):
+    receptor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    Nombre = models.CharField(max_length=200)
+    color = models.CharField(max_length=50)
+    descripcion = models.TextField(max_length=200, null=True)
+    cantidad = models.IntegerField()
+    valorneto = models.IntegerField()
+    fecharecepcion = models.DateField(blank=True, null=True)
+    foto = models.ImageField(upload_to= 'Accesorios')
+
+    def publish(self):
+        self.fecharecepcion = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.modelo
+
