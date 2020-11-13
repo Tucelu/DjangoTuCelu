8706d698a340c82519f396d9ad35fc9a8edd483d
ArielMarihuen <Ar.marihuen@alumnos.duoc.cl>
2020-11-13 18:09:17 -0300

    Algunos error de merge solucionados

--- celu/urls.py
+++ celu/urls.py
@@ -3,7 +3,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    
     path('', views.Celu_home, name='Celu_Home'),
     path('Acce_list', views.Acce_list , name = 'Acce_list'),
     path('Celu_list', views.Celu_list , name = 'Celu_list'),
