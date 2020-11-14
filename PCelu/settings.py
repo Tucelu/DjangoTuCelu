d9b222382b043dc7c16f3e4a80d1a7395a4fdf12
ArielMarihuen <Ar.marihuen@alumnos.duoc.cl>
2020-11-14 00:07:24 -0300

    Subiando A pythonAnyWhere

--- PCelu/settings.py
+++ PCelu/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/Celulares/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'celu/static/picture')
