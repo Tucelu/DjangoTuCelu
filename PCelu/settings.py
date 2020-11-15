8d78e73b273d1ca9d7a4d0b237336e3cc1b0120b
ArielMarihuen <72284828+ArielMarihuen@users.noreply.github.com>
2020-11-15 00:44:02 -0300

    Update settings.py

--- PCelu/settings.py
+++ PCelu/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/producto/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'celu/static/picture')
