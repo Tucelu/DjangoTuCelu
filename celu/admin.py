0dbc7274912052257ac0f194a7731b66717a8266
ArielMarihuen <Ar.marihuen@alumnos.duoc.cl>
2020-11-15 12:49:32 -0300

    Reformulando Proyecto/ recuperando version anterior

--- celu/admin.py
+++ celu/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Producto, Tipo
+from .models import Producto, Tipo, DetalleProducto
 # Register your models here.
  
 admin.site.register(Producto)
 admin.site.register(Tipo)
+admin.site.register(DetalleProducto)
