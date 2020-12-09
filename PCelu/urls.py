9945b6a8b123cb126961ce9863ee49a8e61f5995
ArielMarihuen <Ar.marihuen@alumnos.duoc.cl>
2020-12-09 00:50:26 -0300

    integracion de PWA con serviceworker

--- PCelu/urls.py
+++ PCelu/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('celu.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
+    path('', include('pwa.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
